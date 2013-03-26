4d00c3f0204931f3ec9eaf9ae696b269fb92d90f
Caleb Perkins <calebtperkins@gmail.com>
2013-03-26 12:44:00 -0400

    Cleanup

--- src/org/liicornell/cfr/hadoop/Preprocessor.java
+++ src/org/liicornell/cfr/hadoop/Preprocessor.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.stream.XMLInputFactory;
@@ -16,8 +15,16 @@ import javax.xml.stream.XMLStreamReader;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.mahout.classifier.bayes.XmlInputFormat;
@@ -123,19 +130,6 @@ public class Preprocessor extends Configured implements Tool {
 
 	}
 
-	@SuppressWarnings("unused")
-	private static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-
-		@Override
-		public void reduce(Text key, Iterator<Text> values,
-				OutputCollector<Text, Text> output, Reporter reporter)
-				throws IOException {
-			while (values.hasNext()) {
-				//
-			}
-		}
-	}
-	
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(null, new Preprocessor(), args);
 		System.exit(res);
--- src/org/liicornell/cfr/runner/Runner.java
+++ src/org/liicornell/cfr/runner/Runner.java
@@ -16,21 +16,32 @@ import org.liicornell.cfr.nlp.TripleGenerator;
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
+		// parse arguments
+		if (args.length != 2) {
+			System.err.println("Usage: Runner input_file output_file");
+		}
+		String input = args[0];
+		String output = args[1];
+		
 		ExecutorService pool = Executors.newFixedThreadPool(7);
 		RDFGenerator rdfGenerator = new RDFGenerator();
 		Set<Triple> triples = new HashSet<Triple>();
 
 		SAXBuilder builder = new SAXBuilder();
-		Document doc = builder.build("/tmp/part-1.xml");
+		Document doc = builder.build(input);
 		Element rootNode = doc.getRootElement();
 		ElementFilter filter = new ElementFilter("text");
+		
+		// each text tag is processed separately
 		for (Element c : rootNode.getDescendants(filter)) {
 			pool.execute(new TripleGenerator(triples, c.getText()));
 		}
+		
+		// wait for threads to finish and build RDF file
 		pool.shutdown();
 		pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 		rdfGenerator.buildModel(triples);
-		rdfGenerator.writeTo("/tmp/part-1.rdf.xml");
+		rdfGenerator.writeTo(output);
 	}
 
 }
