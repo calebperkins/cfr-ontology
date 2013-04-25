59750381288ca04c040353fa2e8f43a5fd2faaeb
Caleb Perkins <calebtperkins@gmail.com>
2013-04-24 22:24:59 -0400

    Output URIs instead of IDs

--- datasets/geonames/geonames.py
+++ datasets/geonames/geonames.py
@@ -11,17 +11,21 @@ import sys
 
 sys.stdout = open("geoids.txt", "w")
 
+def toURI(geoID):
+    return u"http://sws.geonames.org/%s/" % geoID
+
 with open("cities15000.txt", "r") as csv_file:
     reader = csv.reader(csv_file, delimiter='\t')
     for row in reader:
         geoid = row[0]
         ascii_name = row[2]
-        print "%s %s" % (geoid, ascii_name)
+        s = "%s|%s" % (toURI(geoid), ascii_name)
+        print s.encode('utf-8')
 
 
 with open("countryInfo.json") as json_file:
     raw_content = json_file.readlines()[0]
     data = json.loads(raw_content)
     for country in data[u"geonames"]:
-        s = "%s %s" % (country[u"geonameId"], country[u"countryName"])
+        s = "%s|%s" % (toURI(country[u"geonameId"]), country[u"countryName"])
         print s.encode('utf-8')
